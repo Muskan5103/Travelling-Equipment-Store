042b12c9bd0f5b15aef6e289cb1432bb23d6027f
muskan5103 <muskanmahajan736@gmail.com>
2026-03-28 23:57:53 +0530

    add STATIC_ROOT for deployment

--- travel_equipment_store/settings.py
+++ travel_equipment_store/settings.py
@@ -85,6 +85,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -183,6 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
